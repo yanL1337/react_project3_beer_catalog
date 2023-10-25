2fe4cd122296b6272b3921308d97313519b82649
Yan Luka Keita <yanlukakeita2000@gmail.com>
2023-10-25 15:06:46 +0200

    test3

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+import { HashRouter, Routes, Route, Link, NavLink } from "react-router-dom";
 import "./App.scss";
 import Home from "./pages/Home";
 import Overview from "./pages/Overview";
@@ -6,13 +6,13 @@ import Details from "./pages/Details";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/beers" element={<Overview />} />
         <Route path="/beers/:id" element={<Details />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
