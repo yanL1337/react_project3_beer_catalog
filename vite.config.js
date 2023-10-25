86fabdd376edacb6bb62c0791a7b66aea0777043
Yan Luka Keita <yanlukakeita2000@gmail.com>
2023-10-25 15:26:32 +0200

    test f

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:
-    process.env.NODE_ENV === "production"
-      ? "/react_project3_beer_catalog/"
-      : "/",
+  base: process.env.NODE_ENV === "production" ? "./" : "/",
 });
